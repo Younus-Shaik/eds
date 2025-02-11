f3e3d1b399bc30bd13c27b49698488d8ba59bdee
Shaik Younus <younus123shaik@gmail.com>
2025-02-11 17:11:14 +0530

    assignment

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,6 +1,44 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
-export default function decorate(block) {
+import {
+  decorateMain,
+} from '../../scripts/scripts.js';
+
+import {
+  loadSections,
+} from '../../scripts/aem.js';
+
+/**
+ * Loads a fragment.
+ * @param {string} path The path to the fragment
+ * @returns {HTMLElement} The root element of the fragment
+ */
+export async function loadFragment(path) {
+  if (path && path.startsWith('/')) {
+    const resp = await fetch(`${path}.plain.html`);
+    if (resp.ok) {
+      const main = document.createElement('main');
+      main.innerHTML = await resp.text();
+
+      // reset base path for media to fragment base
+      const resetAttributeBase = (tag, attr) => {
+        main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
+          elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
+        });
+      };
+      resetAttributeBase('img', 'src');
+      resetAttributeBase('source', 'srcset');
+
+      decorateMain(main);
+      await loadSections(main);
+      return main;
+    }
+  }
+  return null;
+}
+
+
+export default async function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
@@ -15,4 +53,18 @@ export default function decorate(block) {
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
+
+  const link = block.querySelector('.cards a[href="/hero"]');
+  const path = link ? link.getAttribute('href') : block.textContent.trim();
+  const fragment = await loadFragment(path);
+  if (fragment) {
+    const fragmentSection = fragment.querySelector(':scope .section');
+    if (fragmentSection) {
+      block.closest('.section').classList.add(...fragmentSection.classList);
+      console.log(block.closest('.section'))
+      console.log(block.closest('.cards').querySelector('a[href="/hero"]'))
+      block.closest('.cards')?.querySelector('a[href="/hero"]')?.replaceWith(...fragment.childNodes);
+    }
+  }
+  
 }
---
+++ blocks/teaser/teaser.js
@@ -0,0 +1,4 @@
+export default function decorate(block) {
+    const fragment = block.querySelector('.teaser a');
+    console.log(fragment.href);
+}
